f3e467537dfd1abe679b19a5c765a13be6350209
MaxKov86 <max.kov86@ukr.net>
2024-03-07 17:15:02 +0200

    Added files ClientInfo and BankInfo, and updated them

---
+++ src/components/BankInfo.jsx
@@ -0,0 +1,15 @@
+const BankInfo = () => {
+  return (
+    <div>
+      <h1>Bank Info:</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt incidunt
+        odio alias magni necessitatibus, deleniti temporibus, quaerat vero
+        labore eligendi asperiores rerum eos corrupti nihil. Alias magnam
+        consectetur soluta voluptates.
+      </p>
+    </div>
+  );
+};
+
+export default BankInfo;
---
+++ src/components/ClientInfo.jsx
@@ -0,0 +1,15 @@
+const ClientInfo = () => {
+  return (
+    <div>
+      <h1>Client Info:</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt incidunt
+        odio alias magni necessitatibus, deleniti temporibus, quaerat vero
+        labore eligendi asperiores rerum eos corrupti nihil. Alias magnam
+        consectetur soluta voluptates.
+      </p>
+    </div>
+  );
+};
+
+export default ClientInfo;
